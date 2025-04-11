758b7c179cdbe79fa4c4ace8ba79653c6dd0e4b1
vijaykv5 <vijaykv2228@gmail.com>
2025-04-11 12:21:40 +0530

    feat : Implemented Time Checks with error handling

--- anchor/programs/voting/src/lib.rs
+++ anchor/programs/voting/src/lib.rs
@@ -4,6 +4,14 @@ use anchor_lang::prelude::*;
 
 declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 
+#[error_code]
+pub enum VotingError {
+    #[msg("The poll has not started yet")]
+    PollNotStarted,
+    #[msg("The poll has already ended")]
+    PollEnded,
+}
+
 #[program]
 pub mod voting {
     use super::*;
@@ -34,6 +42,18 @@ pub mod voting {
     }
 
     pub fn vote(ctx: Context<Vote>, _candidate_name: String, _poll_id: u64) -> Result<()> {
+        let poll = &ctx.accounts.poll;
+        let current_time = Clock::get()?.unix_timestamp as u64;
+
+        require!(
+            current_time >= poll.poll_start,
+            VotingError::PollNotStarted
+        );
+
+        require!(
+            current_time <= poll.poll_end,
+            VotingError::PollEnded
+        );
         let candidate = &mut ctx.accounts.candidate;
         candidate.candidate_votes += 1;
 
